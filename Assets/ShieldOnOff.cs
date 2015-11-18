dd6954fc7099e109120c029c880e5a0ecffebf93
Nick Junius <njunius@ucsc.edu>
2015-11-18 11:05:33 -0800

    Disabled Shield Collider when off

---
+++ Assets/ShieldOnOff.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldOnOff : MonoBehaviour {
+
+    PlayerController player;
+    // Use this for initialization
+    void Start()
+    {
+        player = (PlayerController) transform.parent.gameObject.GetComponent("PlayerController");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.isShielded())
+        {
+            GetComponent<MeshRenderer>().enabled = true;
+            GetComponent<CapsuleCollider>().enabled = true;
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<CapsuleCollider>().enabled = false;
+        }
+    }
+}
+
